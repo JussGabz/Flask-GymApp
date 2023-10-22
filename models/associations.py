9aabb0f5853c7bc592831e2f1de0b4b062dc373a
JussGabz <gabrieli94@hotmail.co.uk>
2023-10-22 12:28:47 +0100

    split up association table

--- models/associations.py
+++ models/associations.py
@@ -1,12 +1,10 @@
 from sqlalchemy import Table, Column, ForeignKey
-from sqlalchemy.ext.declarative import declarative_base
 
-Base = declarative_base()
-from models.engine import engine
+from . import Base
 
 plan_exercise = Table(
     "plan_exercise",
     Base.metadata,
     Column("exercise_id", ForeignKey("exercises.id")),
-    Column("workoutplan_id", ForeignKey("workout_plan.id"))
+    Column("plan_id", ForeignKey("workout_plan.id"))
 )
